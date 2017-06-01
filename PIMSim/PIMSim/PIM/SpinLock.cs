2428f1d6578705551bbe5d2a5040753f4d611594
Xu Sheng <vinezw@gmail.com>
2017-06-01 20:52:16 +0800

    Spin Lock partitioned by blocks

--- PIMSim/PIMSim/PIM/SpinLock.cs
+++ PIMSim/PIMSim/PIM/SpinLock.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿#region Reference
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SimplePIM.Configs;
+#endregion
 
 namespace SimplePIM.PIM
 {
+    /// <summary>
+    /// Spin Lock Defination
+    /// Spin Lock is an overall data corherence method. By looking at spinlock table,Processors and PIM units can get informations that which block are used.
+    /// </summary>
     public class SpinLock
     {
         #region static information
@@ -20,31 +26,54 @@ namespace SimplePIM.PIM
         private readonly char FALSE = '0';
 
         #endregion
+        /// <summary>
+        /// the page index of an address
+        /// </summary>
         public UInt64 page_index;
+
+        /// <summary>
+        /// Page Size
+        /// </summary>
         public UInt64 size = Config.page_size;
+
+        /// <summary>
+        /// Lock table
+        /// </summary>
         public List<string> lock_table;
+
         public SpinLock()
         {
             page_index = MemorySelecter.get_RAM_size() / size;
 
             lock_table = new List<string>();
+            //foreach block entry, set lock table false.
             for (int i = 0; i < (Int64)(page_index / 64); i++)
                 lock_table.Add(empty);
-            //if()
         }
+        /// <summary>
+        /// Set data locked.
+        /// </summary>
+        /// <param name="addr">Used address</param>
         public void setlock(UInt64 addr)
         {
+            //resize address in case of address is out of range.
             var addr_ = MemorySelecter.resize(addr);
+
             Int64 index_all = (Int64)(addr_ / size);
             int index = (int)(index_all / 64);
             int mod = (int)(index_all % 64);
             setbit(mod, index, true);
         }
+        /// <summary>
+        /// Get current lock state of an address.
+        /// </summary>
+        /// <param name="addr">Used address</param>
+        /// <returns></returns>
         public bool get_lock_state(UInt64 addr)
         {
-           var  addr_ = MemorySelecter.resize(addr);
+            var addr_ = MemorySelecter.resize(addr);
             var index = addr_ / size;
-            Int32 i = (Int32)( index/ 64);
+            Int32 i = (Int32)(index / 64);
             int j = (int)(index % 64);
             //  return lock_table[(Int32)(addr / page_index)];
             if ((lock_table[i].ToArray())[j] == TRUE)
@@ -64,7 +93,12 @@ namespace SimplePIM.PIM
             lock_table[i] = new string(item);
         }
 
-
+        /// <summary>
+        /// Set a bit/
+        /// </summary>
+        /// <param name="index">page index</param>
+        /// <param name="i">page offset</param>
+        /// <param name="lock_">lock state</param>
         public void setbit(Int64 index, Int32 i, bool lock_)
         {
             var data = lock_table[i].ToArray();
@@ -74,6 +108,6 @@ namespace SimplePIM.PIM
 
         }
 
-        
+
     }
 }
